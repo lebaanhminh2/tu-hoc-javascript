e3cc5f79e274970728be1b1706b01ced463ee876
Minh Lee <Lebaanhminh2@gmail.com>
2026-06-21 14:22:45 +0700

    video17

--- hoidanit.js
+++ hoidanit.js
@@ -1,1 +1,1 @@
-console.log("Hello, world with js!")
+console.log("Hello, world with js!");
---
+++ src/video17/video17.js
@@ -0,0 +1,9 @@
+console.log("video17");
+
+var name = "hoidanit";
+
+const name1 = "eric";
+
+let name2 = "eric";
+
+console.log(name);
