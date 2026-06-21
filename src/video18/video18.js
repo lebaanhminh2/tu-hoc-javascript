5b6f504735947b665d3e1233ac180faeca4e29f0
Minh Lee <Lebaanhminh2@gmail.com>
2026-06-21 17:03:38 +0700

    video 18 + thuoc git

---
+++ src/video18/video18.js
@@ -0,0 +1,28 @@
+console.log("video18");
+
+//number
+const myAge = 18;
+const myScore = 9.1;
+
+console.log(myAge);
+console.log(myScore);
+
+//string
+const name = "hoidanit";
+const name1 = "erc";
+const name2 = "javascript";
+
+console.log(name);
+
+//boolean
+const istuideptrai = true;
+console.log(istuideptrai);
+
+//undefined
+let myName;
+console.log(myName);
+
+//null
+const myNull = null;
+console.log(myNull);
+
